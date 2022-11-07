fed8cb910c658b68f9a5fc115c31f63f086f12ad
1152082078@qq.com <zh151832>
2022-11-07 22:02:05 +0800

    init

--- contracts/Web3Nav.sol
+++ contracts/Web3Nav.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract Web3Nav {
     uint public immutable STAKING_LIMIT;
@@ -14,9 +14,10 @@ contract Web3Nav {
     
 
     mapping(address => uint) userStakingInfo;
-    mapping(address => uint) Web3NavAmt;
+    mapping(address => uint) web3NavAmt;
 
-    event Staking(address indexed _from, uint _amt);
+    event Staking(address indexed _addr, uint _amt);
+    event CancelStaking(address indexed _addr, uint _amt);
 
     constructor(uint _stakingLimit, uint _exchangeRate) {
         STAKING_LIMIT = _stakingLimit;
@@ -37,10 +38,29 @@ contract Web3Nav {
         // 参数校验
         // require(msg.value >= STAKING_LIMIT, "staking amount not enough!");
         userStakingInfo[msg.sender] += msg.value;
-        Web3NavAmt[msg.sender] += (msg.value)
+
+        uint mintWeb3NavAmt = msg.value * EXCHANGE_RATE;
+        web3NavAmt[msg.sender] += mintWeb3NavAmt;
+        totalSupply += mintWeb3NavAmt;
 
         emit Staking(msg.sender, msg.value);
     }
 
+    function cancelStaking() external {
+        require(userStakingInfo[msg.sender] > 0, "nothing to withdraw");
+        
+        uint etherAmt = userStakingInfo[msg.sender];
+        userStakingInfo[msg.sender] = 0;
+        uint mintWeb3NavAmt = etherAmt * EXCHANGE_RATE;
+        web3NavAmt[msg.sender] -= mintWeb3NavAmt;
+        totalSupply -= mintWeb3NavAmt;
+
+        (bool succeed,) = msg.sender.call{value: etherAmt}("");
+        require(succeed, "cancelStaking failed!");
+
+        console.log("rewards:", etherAmt);
+
+        emit CancelStaking(msg.sender, etherAmt);
+    }
 
 }
