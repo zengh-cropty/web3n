766cf289aff0087cf79ae71ad0c590a8f2a59cf1
Remix Workspace <>
2022-11-07 21:35:22 +0800

    init

--- contracts/Web3Nav.sol
+++ contracts/Web3Nav.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -8,13 +8,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Web3Nav {
     uint public immutable STAKING_LIMIT;
+    uint public immutable EXCHANGE_RATE;
     address public admin;
+    uint public totalSupply;
+    
 
-    uint public total
+    mapping(address => uint) userStakingInfo;
+    mapping(address => uint) Web3NavAmt;
 
+    event Staking(address indexed _from, uint _amt);
 
-    constructor(uint _stakingLimit) {
+    constructor(uint _stakingLimit, uint _exchangeRate) {
         STAKING_LIMIT = _stakingLimit;
+        EXCHANGE_RATE = _exchangeRate;
         admin = msg.sender;
     }
 
@@ -22,4 +28,19 @@ contract Web3Nav {
         require(msg.sender == admin, "not authorized!");
         _;
     }
+
+    function _isCompleteStaking(address addr) internal view returns (bool) {
+        return userStakingInfo[addr] >= STAKING_LIMIT;
+    }
+
+    function staking () payable public {
+        // 参数校验
+        // require(msg.value >= STAKING_LIMIT, "staking amount not enough!");
+        userStakingInfo[msg.sender] += msg.value;
+        Web3NavAmt[msg.sender] += (msg.value)
+
+        emit Staking(msg.sender, msg.value);
+    }
+
+
 }
