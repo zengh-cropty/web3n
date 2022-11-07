b0283a2886fb260578175ab2a61648ed4a43331e
Remix Workspace <>
2022-11-07 22:33:09 +0800

    init

--- contracts/Web3Nav.sol
+++ contracts/Web3Nav.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
@@ -11,6 +11,7 @@ contract Web3Nav {
     uint public immutable EXCHANGE_RATE;
     address public admin;
     uint public totalSupply;
+    uint public totalStaking;
     
 
     mapping(address => uint) userStakingInfo;
@@ -30,6 +31,7 @@ contract Web3Nav {
         _;
     }
 
+
     function _isCompleteStaking(address addr) internal view returns (bool) {
         return userStakingInfo[addr] >= STAKING_LIMIT;
     }
@@ -39,6 +41,7 @@ contract Web3Nav {
         // require(msg.value >= STAKING_LIMIT, "staking amount not enough!");
         userStakingInfo[msg.sender] += msg.value;
 
+        totalStaking += msg.value;
         uint mintWeb3NavAmt = msg.value * EXCHANGE_RATE;
         web3NavAmt[msg.sender] += mintWeb3NavAmt;
         totalSupply += mintWeb3NavAmt;
@@ -47,13 +50,14 @@ contract Web3Nav {
     }
 
     function cancelStaking() external {
-        require(userStakingInfo[msg.sender] > 0, "nothing to withdraw");
-        
         uint etherAmt = userStakingInfo[msg.sender];
         userStakingInfo[msg.sender] = 0;
+        require(etherAmt > 0, "nothing to withdraw");
+        
         uint mintWeb3NavAmt = etherAmt * EXCHANGE_RATE;
         web3NavAmt[msg.sender] -= mintWeb3NavAmt;
         totalSupply -= mintWeb3NavAmt;
+        totalStaking -= etherAmt;
 
         (bool succeed,) = msg.sender.call{value: etherAmt}("");
         require(succeed, "cancelStaking failed!");
@@ -61,6 +65,10 @@ contract Web3Nav {
         console.log("rewards:", etherAmt);
 
         emit CancelStaking(msg.sender, etherAmt);
+    }
+
+    function getStakingAmt() external view returns (uint) {
+        return userStakingInfo[msg.sender];
     }
 
 }
