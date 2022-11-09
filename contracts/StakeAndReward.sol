9ec41edd69175494c2609d3497f2f5daf4e3458d
1152082078@qq.com <zh151832>
2022-11-10 00:00:35 +0800

    init

--- contracts/StakeAndReward.sol
+++ contracts/StakeAndReward.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Pool  is Ownable {
-
+contract Pool is Ownable {
     //质押token地址
     IERC20 stakeToken;
     //质押奖励token地址
@@ -33,7 +32,11 @@ contract Pool  is Ownable {
     uint256 lastAddUpRewardPerShareAll;
 
     //构造函数
-    constructor(address _stakeTokenAddr, address _rewardTokenAddr, uint256 _rewardPerMin){
+    constructor(
+        address _stakeTokenAddr,
+        address _rewardTokenAddr,
+        uint256 _rewardPerMin
+    ) {
         stakeToken = IERC20(_stakeTokenAddr);
         rewardToken = IERC20(_rewardTokenAddr);
         rewardPerMin = _rewardPerMin;
@@ -44,25 +47,30 @@ contract Pool  is Ownable {
     /// @notice 4. 记录此时msg.sender已经产出的总奖励
     /// @notice 2. 增加msg.sender等量的质押份额
     /// @notice 3. 计算此时每份额累计总产出奖励
-    function stake(uint256 _amount) external 
-    {
-        stakeToken.transferFrom(msg.sender, this.address, _amount); 
+    function stake(uint256 _amount) external {
+        stakeToken.transferFrom(msg.sender, address(this), _amount);
         uint256 currenTotalRewardPerShare = getRewardPerShare();
-        lastAddUpReward[msg.sender] +=  (currenTotalRewardPerShare - lastAddUpRewardPerShare[msg.sender]) * shares[msg.sender];
+        lastAddUpReward[msg.sender] +=
+            (currenTotalRewardPerShare - lastAddUpRewardPerShare[msg.sender]) *
+            shares[msg.sender];
         shares[msg.sender] += _amount;
         updateTotalShare(_amount, 1);
         lastAddUpRewardPerShare[msg.sender] = currenTotalRewardPerShare;
-    } 
+    }
 
     //解除质押，提取token,【外部调用/所有人/不需要支付/读写状态】
     /// @notice 1. _amount必须<=已经质押的份额
     /// @notice 4. 记录此时msg.sender已经产出的总奖励
-    function unStake(uint256 _amount) external 
-    {
-        require(_amount <= shares[msg.sender], "UNSTAKE_AMOUNT_MUST_LESS_SHARES");
-        stakeToken.transferFrom(this.address, msg.sender, _amount); 
+    function unStake(uint256 _amount) external {
+        require(
+            _amount <= shares[msg.sender],
+            "UNSTAKE_AMOUNT_MUST_LESS_SHARES"
+        );
+        stakeToken.transferFrom(address(this), msg.sender, _amount);
         uint256 currenTotalRewardPerShare = getRewardPerShare();
-        lastAddUpReward[msg.sender] +=  (currenTotalRewardPerShare - lastAddUpRewardPerShare[msg.sender]) * shares[msg.sender];
+        lastAddUpReward[msg.sender] +=
+            (currenTotalRewardPerShare - lastAddUpRewardPerShare[msg.sender]) *
+            shares[msg.sender];
         shares[msg.sender] -= _amount;
         updateTotalShare(_amount, 2);
         lastAddUpRewardPerShare[msg.sender] = currenTotalRewardPerShare;
@@ -72,99 +80,89 @@ contract Pool  is Ownable {
     /// @param _amount 更新的数量
     /// @param _type 1增加，其他 减少
     /// @notice 每次更新份额之前，先计算之前的份额累计奖励
-    function updateTotalShare(uint256 _amount, uint256 _type) 
-        internal 
-        onlyOwner 
-    {  
+    function updateTotalShare(uint256 _amount, uint256 _type)
+        internal
+        onlyOwner
+    {
         lastAddUpRewardPerShareAll = getRewardPerShare();
         lastBlockT = block.timestamp;
-        if(_type == 1){
+        if (_type == 1) {
             totalShares += _amount;
-        } else{
+        } else {
             totalShares -= _amount;
         }
     }
 
     //获取截至当前每份额累计产出,【内部调用/合约创建者/不需要支付/只读】
-    /// @notice 1.（当前区块时间戳-具体当前最近一次计算的时间戳） * 每分钟产出奖励 / 60秒 / 总份额  + 距离当前最近一次计算的时候的每份额累计奖励 = 当前每份额累计奖励 
-    /// @notice 2. 更新最近一次计算每份额累计奖励的时间和数量 
-    function getRewardPerShare() 
-        internal 
-        view 
-        onlyOwner 
-        returns(uint256)
-    {  
-        return (block.timestamp - lastBlockT) * rewardPerMin / 60 / totalShares + lastAddUpRewardPerShareAll;
+    /// @notice 1.（当前区块时间戳-具体当前最近一次计算的时间戳） * 每分钟产出奖励 / 60秒 / 总份额  + 距离当前最近一次计算的时候的每份额累计奖励 = 当前每份额累计奖励
+    /// @notice 2. 更新最近一次计算每份额累计奖励的时间和数量
+    function getRewardPerShare() internal view onlyOwner returns (uint256) {
+        return
+            ((block.timestamp - lastBlockT) * rewardPerMin) /
+            60 /
+            totalShares +
+            lastAddUpRewardPerShareAll;
     }
 
     //计算累计奖励,【内部调用/合约创建者/不需要支付/只读】
     /// @notice 仅供内部调用，统一计算规则
-    function getaddupReword(address _address) 
+    function getaddupReword(address _address)
         internal
-        onlyOwner 
-        view 
-        returns(uint256)
+        view
+        onlyOwner
+        returns (uint256)
     {
-        return lastAddUpReward[_address] +  ((getRewardPerShare() - lastAddUpRewardPerShare[_address]) * shares[_address]);
+        return
+            lastAddUpReward[_address] +
+            ((getRewardPerShare() - lastAddUpRewardPerShare[_address]) *
+                shares[_address]);
     }
 
     //计算可提现奖励,【内部调用/合约创建者/不需要支付/只读】
     /// @notice 仅供内部调用，统一计算规则
-    function getWithdrawdReword(address _address) 
+    function getWithdrawdReword(address _address)
         internal
-        onlyOwner 
-        view 
-        returns(uint256)
+        view
+        onlyOwner
+        returns (uint256)
     {
-        return lastAddUpReward[_address] +  ((getRewardPerShare() - lastAddUpRewardPerShare[_address]) * shares[_address]) - withdrawdReward[_address];
+        return
+            lastAddUpReward[_address] +
+            ((getRewardPerShare() - lastAddUpRewardPerShare[_address]) *
+                shares[_address]) -
+            withdrawdReward[_address];
     }
 
     //提现收益,【外部调用/所有人/不需要支付/读写】
     /// @notice 1. 计算截至到当前的累计获得奖励
     /// @notice 2. _amount必须<=(累计获得奖励-已提现奖励)
     /// @notice 3. 提现，提现需要先增加数据，再进行提现操作
-    function withdraw(uint256 _amount) 
-        external 
-    {
-        require(_amount <= getWithdrawdReword(msg.sender), "WITHDRAW_AMOUNT_LESS_ADDUPREWARD");
+    function withdraw(uint256 _amount) external {
+        require(
+            _amount <= getWithdrawdReword(msg.sender),
+            "WITHDRAW_AMOUNT_LESS_ADDUPREWARD"
+        );
         withdrawdReward[msg.sender] += _amount;
-        rewardToken.transferFrom(this.address, msg.sender, _amount); 
+        rewardToken.transferFrom(address(this), msg.sender, _amount);
     }
 
     //获取可提现奖励，【外部调用/所有人/不需要支付】
-    function withdrawdReword() 
-        external
-        view 
-        returns(uint256)
-    {
+    function withdrawdReword() external view returns (uint256) {
         return getWithdrawdReword(msg.sender);
     }
 
     //获取已提现奖励，【外部调用/所有人/不需要支付】
-    function hadWithdrawdReword() 
-        external
-        view 
-        returns(uint256)
-    {
+    function hadWithdrawdReword() external view returns (uint256) {
         return withdrawdReward[msg.sender];
     }
 
     //获取累计奖励，【外部调用/所有人/不需要支付】
-    function addupReword() 
-        external
-        view 
-        returns(uint256)
-    {
+    function addupReword() external view returns (uint256) {
         return getaddupReword(msg.sender);
     }
 
     //获取质押份额,【外部调用/所有人/不需要支付/只读】
-    function getShare() 
-        external
-        view 
-        returns(uint256)
-    {
+    function getShare() external view returns (uint256) {
         return shares[msg.sender];
     }
-
 }
--- test/Lock.ts
+++ test/Lock.ts
@@ -3,47 +3,28 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("Staker", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
+  async function deployStaker() {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const Staker = await ethers.getContractFactory("Staker");
+    const staker = await Staker.deploy();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { staker, owner, otherAccount };
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
+    it("Should staker balance is 0", async function () {
+      const { staker, owner } = await loadFixture(
+        deployStaker
       );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
+      expect(await ethers.provider.getBalance(staker.address)).to.equal(0);
     });
 
     it("Should fail if the unlockTime is not in the future", async function () {
