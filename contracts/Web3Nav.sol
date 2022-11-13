c1646ed700db21fd580f61d305e181bdaacc4125
1152082078@qq.com <zh151832>
2022-11-13 23:30:49 +0800

    init

--- contracts/Web3Nav.sol
+++ contracts/Web3Nav.sol
@@ -5,13 +5,13 @@ import "./ERC20.sol";
 
 contract Web3Nav is ERC20 {
 
-    uint8 internal constant DECIMAL_PLACES = 18;
+    // string constant TOKEN_NAME = "Web3Nav";
+    // string constant TOKEN_SYMBOL = "nav";
 
-    string constant TOKEN_NAME = "Web3Nav";
-    string constant TOKEN_SYMBOL = "nav";
-
-    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
-        
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {
     }
 
-}
+} 
