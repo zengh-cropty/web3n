bd52c271ade8773dd7d0c43c0be65b631ed73ce4
zengh-cropty <1152082078@qq.com>
2022-11-17 00:13:17 +0800

    init

--- contracts/Web3Nav.sol
+++ contracts/Web3Nav.sol
@@ -1,17 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Web3Nav is ERC20 {
+contract Nav is ERC20, AccessControl {
+    // bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    // string constant TOKEN_NAME = "Web3Nav";
-    // string constant TOKEN_SYMBOL = "nav";
-
-    constructor(
-        string memory name_,
-        string memory symbol_
-    ) ERC20(name_, symbol_) {
+    constructor() ERC20("nav", "NAV") {
+        // _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // _grantRole(MINTER_ROLE, msg.sender);
     }
 
-} 
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
